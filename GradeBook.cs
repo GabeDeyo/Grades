9145654b3fd01d717d4b6be5fef8154ed865cd71
Gabe Deyo <gdeyo@ibidata.com>
2017-02-01 16:07:57 -0600

    Final Project

--- GradeBook.cs
+++ GradeBook.cs
@@ -1,71 +1,53 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-namespace Grades {
-	public class GradeBook {
-		public GradeBook() {
-			_name = "Empty";
+namespace Grades
+{
+	public class GradeBook : GradeTracker
+	{
+		public GradeBook()
+		{
+			_name = "Grade Book";
 			grades = new List<float>();
 		}
 
-		private List<float> grades = new List<float>();
-		private string _name;
-
-		public event NameChangedDelegate NameChanged;
-
-		/* Provides no validation
-		public string Name {
-			get;
-			set;
-		}*/
-
-		public string Name {
-			get {
-				return _name;
-			}
-			set {
-				if (string.IsNullOrEmpty(value)) {
-					throw new ArgumentException("Error");
-				}
-
-				if(_name != value) //Shows name IS changing
-				{ 
-					NameChangedEventArgs args = new NameChangedEventArgs();
-					args.ExistingName = _name;
-					args.NewName = value;
-
-					NameChanged(this, args);
-				}
-				_name = value;
-				
-			}
+		public override IEnumerator GetEnumerator()
+		{
+			return grades.GetEnumerator();
 		}
 
-		public GradeStatistics ComputeStatistics() {
+		protected List<float> grades;
+
+		public override GradeStatistics ComputeStatistics()
+		{
 			GradeStatistics stats = new GradeStatistics();
 
 			float sum = 0;
-			foreach(float grade in grades) {
+			foreach(float grade in grades)
+			{
 				stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
 				stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
 
 				sum += grade;
 			}
-			
-			//Careful for this line (in case grades.Count was 0)
+
 			stats.AverageGrade = sum / grades.Count;
 
 			return stats;
 		}
 
-		public void WriteGrades(TextWriter destination) {
-			for (int i = 0; i < grades.Count; i++) {
+		public override void WriteGrades(TextWriter destination)
+		{
+			for (int i = 0; i < grades.Count; i++)
+			{
 				destination.WriteLine(grades[i]);
 			}
 		}
 
-		public void AddGrade(float grade) {
+		public override void AddGrade(float grade)
+		{
 			grades.Add(grade);
 		}
     }
--- Program.cs
+++ Program.cs
@@ -1,67 +1,82 @@
 ﻿using System;
+using System.IO;
 using System.Speech.Synthesis;
 
 namespace Grades
 {
-	class Program {
-		static void Main(string[] args) {
-			//SpeechSynthesizer synth = new SpeechSynthesizer();
-			//synth.Speak("Hello, this is the grade book program.");
-			//synth.Speak("Here are your current grade statistics.");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IGradeTracker book = CreateGradeBook();
 
+			GetBookName(book);
+			AddGrades(book);
+			SaveGrades(book);
+			WriteResults(book);
+		}
 
-			GradeBook book = new GradeBook();
+		private static IGradeTracker CreateGradeBook()
+		{
+			return new GradeBook();
+		}
 
-			// Don't set equal, simply add reference to the method with another delegate
-			book.NameChanged += OnNameChanged;
-			/*book.NameChanged += OnNameChanged2;
-			book.NameChanged += OnNameChanged2;
-			book.NameChanged -= OnNameChanged2;*/
-
-			book.Name = "Gabes Grade Book";
-			book.Name = null; //Isn't applied because I have Property validation
-			book.Name = "Grade Book";
-			book.AddGrade(91);
-			book.AddGrade(89.5f);
-			book.AddGrade(75);
-			book.WriteGrades(Console.Out);
-
+		private static void WriteResults(IGradeTracker book)
+		{
 			GradeStatistics stats = book.ComputeStatistics();
-			Console.WriteLine(book.Name);
+
+			foreach(float grade in book)
+			{
+				Console.WriteLine(grade);
+			}
+
 			WriteResult("Average", stats.AverageGrade);
 			WriteResult("Highest", stats.HighestGrade);
 			WriteResult("Lowest", stats.LowestGrade);
 			WriteResult(stats.Description, stats.LetterGrade);
 		}
 
-		static void OnNameChanged(object sender, NameChangedEventArgs args) {
+		private static void SaveGrades(IGradeTracker book)
+		{
+			using (StreamWriter outputFile = File.CreateText("grades.txt"))
+			{
+				book.WriteGrades(outputFile);
+			}
+		}
+
+		private static void AddGrades(IGradeTracker book)
+		{
+			book.AddGrade(91);
+			book.AddGrade(89.5f);
+			book.AddGrade(75);
+		}
+
+		private static void GetBookName(IGradeTracker book)
+		{
+			try
+			{
+				Console.WriteLine("Enter a name");
+				book.Name = Console.ReadLine();
+			}
+			catch (ArgumentException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		static void OnNameChanged(object sender, NameChangedEventArgs args)
+		{
 			Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}.");
 		}
 
-		/*static void OnNameChanged2(object sender, NameChangedEventArgs args) {
-			Console.WriteLine("***");
-		}*/
-
-
-		static void WriteResult(string description, float result) {
-			/*
-			Standard
-			//Console.WriteLine(description + ": " + result);
-			//Formated
-			Console.WriteLine("{0}: {1}", description, result);
-			//Decimal Format
-			Console.WriteLine("{0}: {1:F2}", description, result);
-			//Currency Format
-			Console.WriteLine("{0}: {1:C2}", description, result);
-			*/
-			//Newest Formating - note we don't need params
+		static void WriteResult(string description, float result)
+		{
 			Console.WriteLine($"{description}: {result}");
 		}
 
-		static void WriteResult(string description, string result) {
+		static void WriteResult(string description, string result)
+		{
 			Console.WriteLine(description + ": " + result);
 		}
-
-		
 	}
 }
